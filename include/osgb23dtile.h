62df0466b89ddec2b8a01aa887d885f44a6b9d2a
llduncan <llduncan@sohu.com>
2018-04-08 22:39:09 +0800

    修改了配置 增加了导出函数定义

--- include/osgb23dtile.h
+++ include/osgb23dtile.h
@@ -1,10 +1,22 @@
 #pragma once
 
-extern "C" void* osgb23dtile_path(
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the PINVOKELIB_EXPORTS
+// symbol defined on the command line. this symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// PINVOKELIB_API functions as being imported from a DLL, wheras this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef TILE_EXPORTS
+#define TILE_API __declspec(dllexport)
+#else
+#define TILE_API __declspec(dllimport)
+#endif
+
+extern "C" TILE_API void* osgb23dtile_path(
 	const char* in_path, const char* out_path,
 	double *box, int* len, int max_lvl);
 
-extern "C" bool osgb23dtile(
+extern "C" TILE_API bool osgb23dtile(
 	const char* in, const char* out);
 
-extern "C" bool osgb2glb(const char* in, const char* out);
+extern "C" TILE_API bool osgb2glb(const char* in, const char* out);
